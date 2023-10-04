097258b45353b97c1e900925953f5887b07a3042
Biiars00 <biiaribeiro60@gmail.com>
2023-10-03 21:45:45 -0300

    feat: building an angular mini blog.

---
+++ src/app/components/small-card/small-card.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card',
+  templateUrl: './small-card.component.html',
+  styleUrls: ['./small-card.component.css']
+})
+export class SmallCardComponent implements OnInit {
+
+@Input()
+smallCardPhoto:string = ''
+
+@Input()
+smallCardTitle:string = ''
+
+@Input()
+smallCardDescription:string = ''
+
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+  }
+
+}
